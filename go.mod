1138b48d26c21235fc8ff61507a6a396ff64cbf3
Damian Smarkucki <damiansmarkucki@gmail.com>
2022-07-16 19:12:12 +0200

    added test suites and checking equality support

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/smarkuck/unittest
+
+go 1.18
---
+++ utils.go
@@ -0,0 +1,107 @@
+package unittest
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+const (
+	BinByte     = "%08b"
+	HexByte     = "%#02x"
+	TwoHexBytes = "%#04x"
+
+	setupFuncName      = "Setup"
+	testPanickedFormat = "test panicked:\n%s"
+	noPanicText        = " (panic didn't return error)"
+	defaultMsg         = "unexpected value"
+
+	outputTemplate = `
+%s:
+    actual:   %s
+    expected: %s`
+)
+
+type T = testing.T
+
+func TestSuite(t *T, suite any) {
+	s := suiteGuts{
+		innerType:  reflect.TypeOf(suite),
+		innerValue: reflect.ValueOf(suite),
+	}
+	s.setupAction = s.getSetupAction()
+	s.runTests(t)
+}
+
+type suiteGuts struct {
+	innerType   reflect.Type
+	innerValue  reflect.Value
+	setupAction func()
+}
+
+func (s *suiteGuts) getSetupAction() func() {
+	m := s.innerValue.MethodByName(setupFuncName)
+	if m.IsValid() {
+		return func() { m.Call([]reflect.Value{}) }
+	}
+	return func() {}
+}
+
+func (s *suiteGuts) runTests(t *T) {
+	for i := 0; i < s.innerType.NumMethod(); i++ {
+		method := s.innerType.Method(i)
+		if method.Name != setupFuncName {
+			s.runTest(t, method)
+		}
+	}
+}
+
+func (s *suiteGuts) runTest(t *T, test reflect.Method) {
+	t.Run(test.Name, func(t *T) {
+		defer recoverOnFail(t)
+		s.setupAction()
+		test.Func.Call([]reflect.Value{
+			s.innerValue, reflect.ValueOf(t)})
+	})
+}
+
+func recoverOnFail(t *T) {
+	err := recover()
+	if err != nil {
+		t.Fatalf(testPanickedFormat, err)
+	}
+}
+
+func ExpectPanicErrEq(t *T, text string, msg ...string) {
+	switch v := recover().(type) {
+	case error:
+		ExpectEq(t, v.Error(), text, msg...)
+	default:
+		msg := getMsg(msg...) + noPanicText
+		signalError(t, v, text, "%v", msg)
+	}
+}
+
+func ExpectEq[V comparable](t *T, v1, v2 V, msg ...string) {
+	ExpectEqf(t, v1, v2, "%v", msg...)
+}
+
+func ExpectEqf[V comparable](t *T,
+	v1, v2 V, format string, msg ...string) {
+	if v1 != v2 {
+		signalError(t, v1, v2, format, getMsg(msg...))
+	}
+}
+
+func signalError(t *T,
+	v1, v2 any, format, msg string) {
+	f := fmt.Sprintf(outputTemplate, msg, format, format)
+	t.Errorf(f, v1, v2)
+}
+
+func getMsg(msg ...string) string {
+	if len(msg) > 0 {
+		return msg[0]
+	}
+	return defaultMsg
+}
